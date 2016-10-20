980b6ba0f17a33f06062943e58ec6af188998cca
Nathan Jensby <njensby@gmail.com>
2016-10-20 10:06:34 -0500

    added mongoose

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+
 var app = express();
+
+mongoose.connect(process.env.DB_CONN);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
